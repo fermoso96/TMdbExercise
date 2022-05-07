f8fe11d4b48576de73c467dc89ab2b9224317a81
Fernando Hermoso Castaño <ferhc96@gmail.com>
2022-05-07 17:44:10 +0200

    Initial configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'folder/Inbox',
     pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   }
 ];
 
@@ -19,4 +15,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
